25935c550e0c76b4cc86e395a0d0876da84f42a2
Ishaan Sheikh <sheikh.ysbh@gmail.com>
2024-05-05 15:32:29 +0530

    chore: removed comments

--- netlify/functions/getVotes.mjs
+++ netlify/functions/getVotes.mjs
@@ -2,13 +2,7 @@ import { getStore } from "@netlify/blobs";
 import { questions } from "@/data/data";
 
 export default async (req, context) => {
-  // let request = await req.json();
-
   const store = getStore("questions");
-
-  // await questions.setJSON("android", [23, 45]);
-  // await questions.setJSON("windows", [223, 145]);
-  // await questions.setJSON("php", [231, 5]);
 
   var resp = [];
 
